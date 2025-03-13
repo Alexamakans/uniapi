96a978c3979551e7a45b6e3276db7a3d6143a530
Alexander Fougner <alexander.fougner@iver.se>
2025-03-13 11:33:12 +0100

    more wip

--- pkg/uniapi/endpoint.go
+++ pkg/uniapi/endpoint.go
@@ -1,7 +1,11 @@
 package uniapi
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"net/http"
 )
 
 var (
@@ -9,13 +13,77 @@ var (
 	ErrUnauthorized    = errors.New("403 forbidden")
 )
 
+type Options struct {
+	// PathExtension is used for APIs that take path parameters.
+	// Example: {baseUrl}/{baseApiPath}/{threats|endpoints}/{id}
+	//                                 ^PathExtension starts here
+	PathExtension string
+	// Query is the query parameters in the format 'key1=value1&key2=value2' etc.
+	Query string
+	// Body is the JSON body to send in the request, if any.
+	Body []byte
+
+	ExtraHeaders map[string]string
+}
+
 type Endpoint[T any] interface {
-	Call() (T, error)
-	getValueInstance() *T
+	Call(method, url string, options *Options) (T, error)
 }
 
 type BaseEndpoint[T any] struct{}
 
-func (o BaseEndpoint[T]) getValueInstance() *T {
-	return new(T)
+// Call constructs and sends an HTTP request using net/http.
+// It returns a result of type T which is decoded from the JSON response.
+func (o *BaseEndpoint[T]) Call(method, url string, options *Options) (T, error) {
+	var result T
+
+	// Build the URL.
+	url = fmt.Sprintf("%s%s", url, options.PathExtension)
+	if options.Query != "" {
+		url = fmt.Sprintf("%s?%s", url, options.Query)
+	}
+
+	// Create the HTTP request.
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(options.Body))
+	if err != nil {
+		return result, err
+	}
+
+	// Optionally, set content type header if body is provided.
+	if len(options.Body) > 0 {
+		req.Header.Set("Content-Type", "application/json")
+	}
+
+	// Set custom headers if provided.
+	for key, value := range options.ExtraHeaders {
+		req.Header.Set(key, value)
+	}
+
+	// Use a default http.Client.
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return result, err
+	}
+	defer resp.Body.Close()
+
+	// Check for common HTTP error status codes.
+	switch resp.StatusCode {
+	case http.StatusUnauthorized:
+		return result, ErrUnauthenticated
+	case http.StatusForbidden:
+		return result, ErrUnauthorized
+	}
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return result, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	// Decode JSON response into the generic type T.
+	decoder := json.NewDecoder(resp.Body)
+	if err := decoder.Decode(&result); err != nil {
+		return result, err
+	}
+
+	return result, nil
 }
--- pkg/uniapi/service.go
+++ pkg/uniapi/service.go
@@ -2,44 +2,81 @@ package uniapi
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
+	"reflect"
 )
 
-type Options struct {
-	Query string
-	Body  []byte
-
-	ExtraHeaders map[string]string
-}
+type (
+	anyEndpoint           = any
+	anyEndpointReturnType = any
+)
 
 var (
-	ErrNoSuchEndpoint = errors.New("no endpoint registered")
-	ErrNoSuchMethod   = errors.New("no endpoint registered")
+	ErrNoSuchEndpoint  = errors.New("no endpoint registered")
+	ErrInvalidEndpoint = errors.New("invalid endpoint")
 )
 
 type Service interface {
-	AddEndpoint(method, path string, endpoint any)
+	AddEndpoint(method, path string, endpoint anyEndpoint)
 
-	Get(path string, options Options) (any, error)
+	Get(path string, options Options) (anyEndpointReturnType, error)
+	Post(path string, options Options) (anyEndpointReturnType, error)
 }
 
 func NewService(baseUrl string, middlewares ...Middleware) Service {
 	return &service{
-		middlewares: middlewares,
+		middlewares:   middlewares,
+		getEndpoints:  make(map[string]anyEndpoint),
+		postEndpoints: make(map[string]anyEndpoint),
 	}
 }
 
 type service struct {
-	middlewares []Middleware
-	endpoints   map[string]map[string]any
+	middlewares   []Middleware
+	getEndpoints  map[string]anyEndpoint
+	postEndpoints map[string]anyEndpoint
 }
 
-func (o *service) AddEndpoint(method, path string, endpoint any) {
+func (o *service) AddEndpoint(method, path string, endpoint anyEndpoint) {
 }
 
-func (o *service) Get(path string) (any, error) {
-	methodGroup, ok := o.endpoints[http.MethodGet]
+func (o *service) Get(path string, options *Options) (any, error) {
+	endpoint, ok := o.getEndpoints[path]
 	if !ok {
-		return nil, ErrNoSuchEndpoint
+		return nil, fmt.Errorf("GET %s: %w", path, ErrNoSuchEndpoint)
 	}
+	callField := reflect.ValueOf(endpoint).FieldByName("Call")
+	if !callField.IsValid() {
+		return nil, fmt.Errorf("GET %s: Call property is invalid, must be a method: %w", path, ErrInvalidEndpoint)
+	}
+
+	values := callField.Call([]reflect.Value{reflect.ValueOf(http.MethodGet), reflect.ValueOf(options)})
+	result := values[0].Interface()
+	err := values[1].Interface().(error)
+	if err != nil {
+		return nil, err
+	}
+}
+
+func Call[T any](service Service, method, path string, options Options) (*T, error) {
+	var result any
+	var err error
+	switch method {
+	case http.MethodGet:
+		result, err = service.Get(path, options)
+	case http.MethodPost:
+		result, err = service.Post(path, options)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	castResult, ok := result.(*T)
+	if !ok {
+		return nil, fmt.Errorf("unable to cast result to %q", reflect.TypeOf(*new(T)).Name())
+	}
+
+	return castResult, nil
 }
